d75d15ce2c0f46f60c33749c22f02b318b625316
Ajdin Redzepovac <aredzepovac@softraysolutions.com>
2020-11-05 23:43:17 +0100

    working with input, radio button, text area elements

---
+++ Project1/src/guru99Project/executeClassForGuru99.java
@@ -0,0 +1,14 @@
+package guru99Project;
+
+public class executeClassForGuru99 {
+
+	public static void main(String[] args) {
+		
+		guru99Class execute = new guru99Class();
+		
+		execute.invokeBrowser();
+		execute.login("mngr293551", "gUbasyt");
+		execute.addCustomer();
+	}
+
+}
---
+++ Project1/src/guru99Project/guru99Class.java
@@ -0,0 +1,52 @@
+package guru99Project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class guru99Class {
+	
+	ChromeDriver driver;
+	String url = "http://demo.guru99.com/v4/";
+	
+	public void invokeBrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", 
+				   "C:\\Folder for testing\\JARS-selenium\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get(url);
+		
+	}
+	
+	public void login(String username, String password) {
+		
+		WebElement usernameElement = driver.findElement(By.name("uid"));
+		
+		usernameElement.sendKeys(username);
+		
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.name("btnLogin")).click();
+	}
+	
+	public void addCustomer() {
+		
+		driver.findElement(By.linkText("New Customer")).click();
+		driver.findElement(By.name("name")).sendKeys("Test");
+		driver.findElement(By.xpath("//input[@value='f']")).click();
+		driver.findElement(By.name("dob")).sendKeys("02/14/1990");
+		driver.findElement(By.name("addr")).sendKeys("this is address");
+		driver.findElement(By.name("city")).sendKeys("Sarajevo");
+		driver.findElement(By.name("state")).sendKeys("Bosna i Hercegovina");
+		driver.findElement(By.name("pinno")).sendKeys("710000");
+		driver.findElement(By.name("telephoneno")).sendKeys("1234567890");
+		
+		String emailid = "Test" + System.currentTimeMillis() + "@name.com";
+		driver.findElement(By.name("emailid")).sendKeys(emailid);
+		
+		driver.findElement(By.name("password")).sendKeys("thisisnewpassword1");
+		driver.findElement(By.name("sub")).click();
+	}
+
+}
